e01173029bd55853ab0ca01f1311998f5e3fad50
wiceum <wiceum@gmail.com>
2022-01-01 20:43:06 +0500

    Загрузка картинки (№10)

---
+++ media_handler.php
@@ -0,0 +1,14 @@
+<?php
+    require_once 'functions.php';
+    session_start();
+
+    $user_id = $_POST['user_id'];
+    $image = $_FILES['avatar'];
+
+    $current_avatar = $_POST['current_avatar'];
+    unlink('img/demo/avatars/'.$current_avatar);
+
+    upload_avatar($image, $user_id);
+
+    set_flash_message('success', 'Профиль успешно обновлен!');
+    redirect_to('page_profile.php?profile_id='.$user_id);
